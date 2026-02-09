b631c70839fd061c8f13361e5564c67530fa952c
MIGUEL RIBEIRO DOS REIS <miguel.rreis@senacsp.edu.br>
2026-02-09 08:51:50 -0300

    adicionei uma pausa exercicio

---
+++ Pausa-exercicio2.php
@@ -0,0 +1,63 @@
+<?php
+
+
+$estadosBrasil = [
+    'AC' => 'Acre',
+    'AL' => 'Alagoas',
+    'AP' => 'Amapá',
+    'AM' => 'Amazonas',
+    'BA' => 'Bahia',
+    'CE' => 'Ceará',
+    'DF' => 'Distrito Federal',
+    'ES' => 'Espírito Santo',
+    'GO' => 'Goiás',
+    'MA' => 'Maranhão',
+    'MT' => 'Mato Grosso',
+    'MS' => 'Mato Grosso do Sul',
+    'MG' => 'Minas Gerais',
+    'PA' => 'Pará',
+    'PB' => 'Paraíba',
+    'PR' => 'Paraná',
+    'PE' => 'Pernambuco',
+    'PI' => 'Piauí',
+    'RJ' => 'Rio de Janeiro',
+    'RN' => 'Rio Grande do Norte',
+    'RS' => 'Rio Grande do Sul',
+    'RO' => 'Rondônia',
+    'RR' => 'Roraima',
+    'SC' => 'Santa Catarina',
+    'SP' => 'São Paulo',
+    'SE' => 'Sergipe',
+    'TO' => 'Tocantins'
+    
+];
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estados do Brasil</title>
+</head>
+<body>
+
+    <label for="estado">Selecione um Estado:</label>
+    
+      <select id="estado" name="estado">
+    
+
+       <?php foreach ($estadosBrasil as $sigla => $nome): ?> 
+             <option value="<?= $sigla ?>"> <?= $nome ?> </option> 
+        <?php endforeach; ?>
+
+      </select>
+      <button type="submit">Enviar</button>
+
+   
+        
+         
+    
+
+</body>
+</html>
---
+++ Pausa-exercicio3.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+$alunos = [
+    ["nome" => "Ana", "idade" => 18, "nota" => 8.5],
+    ["nome" => "Bruno", "idade" => 20, "nota" => 7.0],
+    ["nome" => "Carlos", "idade" => 19, "nota" => 9.2],
+    ["nome" => "Daniela", "idade" => 21, "nota" => 6.8],
+    ["nome" => "Eduardo", "idade" => 22, "nota" => 5.9],
+    ["nome" => "Fernanda", "idade" => 18, "nota" => 7.8],
+    ["nome" => "Gabriel", "idade" => 20, "nota" => 8.1],
+    ["nome" => "Helena", "idade" => 19, "nota" => 9.0],
+    ["nome" => "Igor", "idade" => 23, "nota" => 6.4],
+    ["nome" => "Juliana", "idade" => 21, "nota" => 7.5],
+    ["nome" => "Lucas", "idade" => 20, "nota" => 8.9],
+    ["nome" => "Mariana", "idade" => 18, "nota" => 9.4],
+];
+
+    
+
+
+
+?>
